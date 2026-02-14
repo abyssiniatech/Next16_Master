91dfecc669c11e6212ab069e816eaade11c24611
abyssiniatech <surafelmengist2015@gmail.com>
2026-02-14 15:13:36 +0300

    the intial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+  "use client"
+import Button from "@/components/reusability/Button";
+
+const Page = () => {
+  return (
+    <div className="p-6">
+      <Button />
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/components/reusablity/Button.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function Button(){
+    return(
+        <div>
+            <button onCliick={()=>console.log("click me")}>Click me</butoon>
+        </div>
+    )
+}
